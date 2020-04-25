5dbefc37f1e45070c383225a937d6ad2d589a57f
martatomalska <tomalska.marta@gmail.com>
2020-04-25 13:57:16 +0200

    added first method

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
     <div v-else>Twój adres e-mail jest stanowczo za długi.</div>
 
     <input type="email" v-model="email">
+    <button @click="alertMyEmail()">Wyświetl mój e-mail w alercie</button>
 
   </div>
 </template>
@@ -19,6 +20,11 @@ export default {
     return {
       email: ''
     };
+  },
+  methods: {
+    alertMyEmail() {
+      alert(this.email);
+    }
   }
 }
 </script>
